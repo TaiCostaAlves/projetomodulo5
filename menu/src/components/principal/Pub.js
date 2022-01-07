9408b12b44feea87e6d65b09138c674e2e009bf4
IsabellaSSantos <bella.sales@hotmail.com>
2022-01-07 01:22:57 -0300

    Começando a estilizar

---
+++ menu/src/components/principal/Pub.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Title } from '../../css/styles';
+
+
+export default function Pub() {
+ 
+
+
+    return (
+        <>
+           <Title>Olá Pub!</Title>
+        </>
+    );
+}
