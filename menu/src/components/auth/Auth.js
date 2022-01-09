7282df6ca08a5abfebded0241f1576ef8e04f6c6
FernandoAgostini28 <fernando.agostini.28@gmail.com>
2022-01-08 23:25:04 -0300

    local storage

--- menu/src/components/auth/Auth.js
+++ menu/src/components/auth/Auth.js
@@ -1,7 +1,7 @@
 export const TOKEN_KEY = '@app-token'
 export const ID_USUARIO = '@id-usuario'
-export const NOME_USUARIO = '@nome-usuario'
-export const USER_TYPE = '@user-type'
+//export const NOME_USUARIO = '@nome-usuario'
+//export const USER_TYPE = '@user-type'
 
 export const login = token => { localStorage.setItem(TOKEN_KEY, token) }
 export const logout = () => { localStorage.clear() }
@@ -9,11 +9,11 @@ export const logout = () => { localStorage.clear() }
 export const setIdUsuario = id => { localStorage.setItem(ID_USUARIO, id) }
 export const getIdUsuario = () => { localStorage.getItem(ID_USUARIO) }
 
-export const setNomeUsuario = nome => { localStorage.setItem(NOME_USUARIO, nome) }
-export const getNomeUsuario = () => { localStorage.getItem(NOME_USUARIO) }
+// export const setNomeUsuario = nome => { localStorage.setItem(NOME_USUARIO, nome) }
+// export const getNomeUsuario = () => { localStorage.getItem(NOME_USUARIO) }
 
-export const setTypeUsuario = type => { localStorage.setItem(USER_TYPE, type) }
-export const getTypeUsuario = () => { localStorage.getItem(USER_TYPE) }
+// export const setTypeUsuario = type => { localStorage.setItem(USER_TYPE, type) }
+// export const getTypeUsuario = () => { localStorage.getItem(USER_TYPE) }
 
 export const getToken = () => localStorage.getItem(TOKEN_KEY)
 
